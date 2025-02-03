4299cca89b3d993bf300ac80298f02cd95806d31
Mario Franca <35295805+SergJohn@users.noreply.github.com>
2025-02-03 11:06:44 +0000

    Updates on reply_on_solution.rb

--- reply_on_solution/reply_on_solution.rb
+++ reply_on_solution/reply_on_solution.rb
@@ -10,13 +10,24 @@ DiscourseAutomation::Scriptable.add(DiscourseAutomation::Scripts::REPLY_ON_SOLUT
   script do |context, fields, automation|
     Rails.logger.warn("DEBUG Automation Context: #{context.inspect}") # Debug line
     # post = context["post"]
-    topic = context["accepted_post_id"]
-    unless topic
-      Rails.logger.error("No topic found in context!")
-      return
+    # topic = context["accepted_post_id"]
+    # unless post
+    #   Rails.logger.error("No post found in context!")
+    #   return
+    # end
+    accepted_post_id = context["accepted_post_id"]
+    accepted_post = Post.find_by(id: accepted_post_id)
+
+    unless accepted_post
+      Rails.logger.error("Accepted post with id #{accepted_post_id} was not found.")
+      next
     end
-    Rails.logger.warn("This is the value of the variable topic: #{topic}")
-    # topic = post.topic
+
+    topic = accepted_post.topic
+    Rails.logger.warn("This is the value of the topic id: #{topic.id}")
+    # Rails.logger.warn("This is the value of the variable topic: #{topic}")
+    # topicid = post.topic.id
+    # Rails.logger.warn("Topic ID: #{topic_id}")
     reply_text = fields.dig("reply_text", "value")
 
     # Post a reply in the topic where a solution was marked
@@ -29,7 +40,7 @@ DiscourseAutomation::Scriptable.add(DiscourseAutomation::Scripts::REPLY_ON_SOLUT
     begin
       PostCreator.create!(
         Discourse.system_user,
-        topic_id: topic,
+        topic_id: topic.id,
         raw: reply_text || "A solution has been marked for this topic!",
       )
     rescue => e
