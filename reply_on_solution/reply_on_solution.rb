33d1678b1937012486e61f7b8e95b07670757e3d
Mario Franca <35295805+SergJohn@users.noreply.github.com>
2025-08-30 13:30:37 +0100

    Update reply_on_solution.rb

--- reply_on_solution/reply_on_solution.rb
+++ reply_on_solution/reply_on_solution.rb
@@ -1,48 +1,31 @@
 # frozen_string_literal: true
 
-# name: reply_on_solution
-# about: An sample scratch autom
-# version: 0.0.1
-# authors: mrosa
+# name: reply-on-solution
+# about: Replies to topics when a solution is accepted
+# version: 0.1
+# authors: SergJohn
 
 enabled_site_setting :reply_on_solution_enabled
 
 after_initialize do
-    if defined?(DiscourseAutomation)
-        on(:first_accepted_solution) do |topic, user|
-            DiscourseAutomation::Automation
-                .where(enabled: true, trigger: "first_accepted_solution")
-                .find_each do |automation|
-                automation.trigger!(
-                    "kind" => "first_accepted_solution",
-                    "usernames" => [user.username],
-                    "topic" => topic,
-                    "placeholders" => {
-                        "sender_username" => user.username,
-                        "word" => "solution",
-                    },
-                )
-            end
-        end
-    end
+  if defined?(DiscourseAutomation) && defined?(DiscourseSolved)
+    add_automation_scriptable("reply_on_solution") do
+      field :reply_text, component: :text
+      version 1
+      triggerables %i[first_accepted_solution]
 
-    DiscourseAutomation::Scriptable::POST_REPLY = "post_reply"
-    add_automation_scriptable(DiscourseAutomation::Scriptable::POST_REPLY) 
-    do
-    field :topic_id, component: :text
-    field :reply_text, component: :text
-    triggerables [:first_accepted_solution]
+      script do |context, fields, automation|
+        topic = context["topic"]
+        reply_text = fields.dig("reply_text", "value") || "Solution accepted!"
 
-    script do |context, fields, automation|
-    if automation.script == "post_reply"
-        begin
-            PostCreator.create!(
-                Discourse.system_user,
-                topic_id: topic.id,
-                raw: reply_text || "A solution has been marked for this topic!",
-            )
-        rescue => e
-            Rails.logger.error("POST CREATION FAILED: #{e.message}\n#{e.backtrace.join("\n")}")
+        if topic && !topic.closed?
+          PostCreator.create!(
+            Discourse.system_user,
+            topic_id: topic.id,
+            raw: reply_text
+          )
         end
+      end
     end
+  end
 end
