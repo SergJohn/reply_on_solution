98db46814ba7fe9907ab6d9444cc4fda159bb025
Mario Franca <35295805+SergJohn@users.noreply.github.com>
2025-09-01 14:41:48 +0100

    Update reply_on_solution.rb

--- reply_on_solution/reply_on_solution.rb
+++ reply_on_solution/reply_on_solution.rb
@@ -8,22 +8,35 @@
 enabled_site_setting :reply_on_solution_enabled
 
 after_initialize do
-  if defined?(DiscourseAutomation) && defined?(DiscourseSolved)
+  if defined?(DiscourseAutomation)
     add_automation_scriptable("reply_on_solution") do
-      field :reply_text, component: :text
+      field :reply_text, component: :message
+      
       version 1
-      triggerables %i[first_accepted_solution]
+      
+      triggerables [:first_accepted_solution] if defined?(DiscourseSolved)
 
       script do |context, fields, automation|
-        topic = context["topic"]
+
+        accepted_post_id = context["accepted_post_id"]
+        accepted_post = Post.find_by(id: accepted_post_id)
         reply_text = fields.dig("reply_text", "value") || "Solution accepted!"
 
-        if topic && !topic.closed?
+        unless accepted_post
+          Rails.logger.error("Accepted post with id #{accepted_post_id} was not found.")
+          next
+        end
+    
+        topic = accepted_post.topic
+    
+        begin
           PostCreator.create!(
             Discourse.system_user,
             topic_id: topic.id,
-            raw: reply_text
+            raw: reply_text,
           )
+        rescue => e
+          Rails.logger.error("POST CREATION FAILED: #{e.message}\n#{e.backtrace.join("\n")}")
         end
       end
     end
